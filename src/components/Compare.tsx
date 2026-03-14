994ab7c3944e716a2eeb685418cecac883f41355
James <z5309441@ad.unsw.edu.au>
2026-03-15 02:42:54 +1100

    adding rr to card header

--- src/components/Compare.tsx
+++ src/components/Compare.tsx
@@ -1,17 +1,17 @@
 
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 const users = [
-  { name: 'imareeq', repos: 67, commits: 67, followers: 67, stars: 67, rank: 1 },
-  { name: 'joshkitten', repos: 67, commits: 67, followers: 67, stars: 67, rank: 2 },
+  { name: 'imareeq', repos: 67, commits: 67, followers: 67, stars: 67, rank: 1, rr: 5500 },
+  { name: 'joshkitten', repos: 67, commits: 67, followers: 67, stars: 67, rank: 2, rr: 4500 },
 ]
 export default function Compare() {
   return (
     <main className="flex flex-row gap-2">
       {users.map(user => (
-        <Card className="w-96"> 
+        <Card key={user.name} className="w-96"> 
           <CardHeader>
             <CardTitle className="text-2xl">{user.name}</CardTitle>
-            <CardDescription className="text-primary">Rank {user.rank}</CardDescription>
+            <CardDescription className="text-primary">#{user.rank} - {user.rr} RR </CardDescription>
           </CardHeader>
           <CardContent className="text-base">
             <div className="flex justify-between items-center py-1">
