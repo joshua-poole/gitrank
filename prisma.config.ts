e0eb9dd78394b5223bc9b8d8e1b19aca8afcfece
Josh Poole <joshpoole6@gmail.com>
2026-03-15 02:44:27 +1100

    fix: run db migrate in dev so prod can migrate correctly

--- prisma.config.ts
+++ prisma.config.ts
@@ -6,7 +6,7 @@ dotenv.config()
 export default defineConfig({
   schema: './prisma/schema.prisma',
   migrations: {
-    path: './generated/migrations',
+    path: './prisma/migrations',
     seed: 'tsx prisma/seed.ts',
   },
   datasource: {
